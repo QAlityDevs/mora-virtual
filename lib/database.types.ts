eb1c47ed51387fe4857d89019853d30ddb86e17a
caverav <camilo@fvv.cl>
2025-06-02 21:13:32 -0400

    ✨ (database.types.ts): update database types to match the new schema

--- lib/database.types.ts
+++ lib/database.types.ts
@@ -1,213 +1,464 @@
-export type Json = string | number | boolean | null | { [key: string]: Json | undefined } | Json[]
-
-export interface Database {
+export type Database = {
   public: {
     Tables: {
-      users: {
-        Row: {
-          id: string
-          created_at: string
-          name: string
-          role: "admin" | "user" | "actor"
-        }
-        Insert: {
-          id: string
-          created_at?: string
-          name: string
-          role?: "admin" | "user" | "actor"
-        }
-        Update: {
-          id?: string
-          created_at?: string
-          name?: string
-          role?: "admin" | "user" | "actor"
-        }
-      }
-      events: {
-        Row: {
-          id: string
-          created_at: string
-          name: string
-          description: string
-          date: string
-          time: string
-          image_url: string
-          sale_start_time: string
-          status: "upcoming" | "active" | "completed"
-        }
-        Insert: {
-          id?: string
-          created_at?: string
-          name: string
-          description: string
-          date: string
-          time: string
-          image_url?: string
-          sale_start_time: string
-          status?: "upcoming" | "active" | "completed"
-        }
-        Update: {
-          id?: string
-          created_at?: string
-          name?: string
-          description?: string
-          date?: string
-          time?: string
-          image_url?: string
-          sale_start_time?: string
-          status?: "upcoming" | "active" | "completed"
-        }
-      }
       actors: {
         Row: {
-          id: string
-          created_at: string
-          name: string
           bio: string
-          photo_url: string
+          created_at: string | null
+          id: string
+          name: string
+          photo_url: string | null
         }
         Insert: {
-          id?: string
-          created_at?: string
-          name: string
           bio: string
-          photo_url?: string
+          created_at?: string | null
+          id?: string
+          name: string
+          photo_url?: string | null
         }
         Update: {
-          id?: string
-          created_at?: string
-          name?: string
           bio?: string
-          photo_url?: string
+          created_at?: string | null
+          id?: string
+          name?: string
+          photo_url?: string | null
         }
+        Relationships: []
       }
       event_actors: {
         Row: {
-          id: string
-          event_id: string
           actor_id: string
+          event_id: string
+          id: string
         }
         Insert: {
-          id?: string
-          event_id: string
           actor_id: string
+          event_id: string
+          id?: string
         }
         Update: {
-          id?: string
-          event_id?: string
           actor_id?: string
+          event_id?: string
+          id?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "event_actors_actor_id_fkey"
+            columns: ["actor_id"]
+            isOneToOne: false
+            referencedRelation: "actors"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "event_actors_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+        ]
       }
-      queue: {
+      events: {
         Row: {
+          created_at: string | null
+          date: string
+          description: string
           id: string
+          image_url: string | null
+          name: string
+          sale_start_time: string
+          status: string | null
+          time: string
+        }
+        Insert: {
+          created_at?: string | null
+          date: string
+          description: string
+          id?: string
+          image_url?: string | null
+          name: string
+          sale_start_time: string
+          status?: string | null
+          time: string
+        }
+        Update: {
+          created_at?: string | null
+          date?: string
+          description?: string
+          id?: string
+          image_url?: string | null
+          name?: string
+          sale_start_time?: string
+          status?: string | null
+          time?: string
+        }
+        Relationships: []
+      }
+      forum_messages: {
+        Row: {
+          content: string
           created_at: string
-          user_id: string
-          event_id: string
-          position: number
-          status: "waiting" | "active" | "completed" | "expired"
-          token: string
-        }
-        Insert: {
-          id?: string
-          created_at?: string
-          user_id: string
-          event_id: string
-          position: number
-          status?: "waiting" | "active" | "completed" | "expired"
-          token: string
-        }
-        Update: {
-          id?: string
-          created_at?: string
-          user_id?: string
-          event_id?: string
-          position?: number
-          status?: "waiting" | "active" | "completed" | "expired"
-          token?: string
-        }
-      }
-      seats: {
-        Row: {
+          forum_id: string
           id: string
-          event_id: string
-          row: string
-          number: number
-          price: number
-          status: "available" | "reserved" | "sold"
+          user_id: string
         }
         Insert: {
+          content: string
+          created_at?: string
+          forum_id: string
           id?: string
-          event_id: string
-          row: string
-          number: number
-          price: number
-          status?: "available" | "reserved" | "sold"
+          user_id: string
         }
         Update: {
-          id?: string
-          event_id?: string
-          row?: string
-          number?: number
-          price?: number
-          status?: "available" | "reserved" | "sold"
-        }
-      }
-      tickets: {
-        Row: {
-          id: string
-          created_at: string
-          user_id: string
-          event_id: string
-          seat_id: string
-          purchase_date: string
-          status: "reserved" | "purchased" | "cancelled"
-        }
-        Insert: {
-          id?: string
+          content?: string
           created_at?: string
-          user_id: string
-          event_id: string
-          seat_id: string
-          purchase_date?: string
-          status?: "reserved" | "purchased" | "cancelled"
-        }
-        Update: {
+          forum_id?: string
           id?: string
-          created_at?: string
           user_id?: string
-          event_id?: string
-          seat_id?: string
-          purchase_date?: string
-          status?: "reserved" | "purchased" | "cancelled"
         }
+        Relationships: [
+          {
+            foreignKeyName: "forum_messages_forum_id_fkey"
+            columns: ["forum_id"]
+            isOneToOne: false
+            referencedRelation: "forums"
+            referencedColumns: ["id"]
+          },
+        ]
       }
       forum_posts: {
         Row: {
-          id: string
-          created_at: string
-          event_id: string
-          user_id: string
           content: string
+          created_at: string | null
+          event_id: string
+          id: string
           parent_id: string | null
+          user_id: string
         }
         Insert: {
-          id?: string
-          created_at?: string
-          event_id: string
-          user_id: string
           content: string
+          created_at?: string | null
+          event_id: string
+          id?: string
           parent_id?: string | null
+          user_id: string
         }
         Update: {
+          content?: string
+          created_at?: string | null
+          event_id?: string
           id?: string
+          parent_id?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "forum_posts_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "forum_posts_parent_id_fkey"
+            columns: ["parent_id"]
+            isOneToOne: false
+            referencedRelation: "forum_posts"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      forums: {
+        Row: {
+          created_at: string
+          event_id: string
+          id: string
+        }
+        Insert: {
+          created_at?: string
+          event_id: string
+          id?: string
+        }
+        Update: {
           created_at?: string
           event_id?: string
-          user_id?: string
-          content?: string
-          parent_id?: string | null
+          id?: string
         }
+        Relationships: [
+          {
+            foreignKeyName: "forums_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+        ]
       }
+      queue: {
+        Row: {
+          created_at: string | null
+          event_id: string
+          id: string
+          position: number
+          status: string | null
+          token: string
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          event_id: string
+          id?: string
+          position: number
+          status?: string | null
+          token: string
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          event_id?: string
+          id?: string
+          position?: number
+          status?: string | null
+          token?: string
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "queue_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      seats: {
+        Row: {
+          event_id: string
+          id: string
+          number: number
+          price: number
+          row: string
+          status: string | null
+        }
+        Insert: {
+          event_id: string
+          id?: string
+          number: number
+          price: number
+          row: string
+          status?: string | null
+        }
+        Update: {
+          event_id?: string
+          id?: string
+          number?: number
+          price?: number
+          row?: string
+          status?: string | null
+        }
+        Relationships: [
+          {
+            foreignKeyName: "seats_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      tickets: {
+        Row: {
+          created_at: string | null
+          event_id: string
+          id: string
+          purchase_date: string | null
+          seat_id: string
+          status: string | null
+          user_id: string
+        }
+        Insert: {
+          created_at?: string | null
+          event_id: string
+          id?: string
+          purchase_date?: string | null
+          seat_id: string
+          status?: string | null
+          user_id: string
+        }
+        Update: {
+          created_at?: string | null
+          event_id?: string
+          id?: string
+          purchase_date?: string | null
+          seat_id?: string
+          status?: string | null
+          user_id?: string
+        }
+        Relationships: [
+          {
+            foreignKeyName: "tickets_event_id_fkey"
+            columns: ["event_id"]
+            isOneToOne: false
+            referencedRelation: "events"
+            referencedColumns: ["id"]
+          },
+          {
+            foreignKeyName: "tickets_seat_id_fkey"
+            columns: ["seat_id"]
+            isOneToOne: false
+            referencedRelation: "seats"
+            referencedColumns: ["id"]
+          },
+        ]
+      }
+      users: {
+        Row: {
+          created_at: string | null
+          id: string
+          name: string
+          role: string | null
+        }
+        Insert: {
+          created_at?: string | null
+          id: string
+          name: string
+          role?: string | null
+        }
+        Update: {
+          created_at?: string | null
+          id?: string
+          name?: string
+          role?: string | null
+        }
+        Relationships: []
+      }
+    }
+    Views: {
+      [_ in never]: never
+    }
+    Functions: {
+      is_admin: {
+        Args: Record<PropertyKey, never>
+        Returns: boolean
+      }
+    }
+    Enums: {
+      [_ in never]: never
+    }
+    CompositeTypes: {
+      [_ in never]: never
     }
   }
 }
+
+type DefaultSchema = Database[Extract<keyof Database, "public">]
+
+export type Tables<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof (DefaultSchema["Tables"] & DefaultSchema["Views"])
+    | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+        Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? (Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"] &
+      Database[DefaultSchemaTableNameOrOptions["schema"]]["Views"])[TableName] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])
+    ? (DefaultSchema["Tables"] &
+        DefaultSchema["Views"])[DefaultSchemaTableNameOrOptions] extends {
+        Row: infer R
+      }
+      ? R
+      : never
+    : never
+
+export type TablesInsert<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Insert: infer I
+      }
+      ? I
+      : never
+    : never
+
+export type TablesUpdate<
+  DefaultSchemaTableNameOrOptions extends
+    | keyof DefaultSchema["Tables"]
+    | { schema: keyof Database },
+  TableName extends DefaultSchemaTableNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"]
+    : never = never,
+> = DefaultSchemaTableNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaTableNameOrOptions["schema"]]["Tables"][TableName] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : DefaultSchemaTableNameOrOptions extends keyof DefaultSchema["Tables"]
+    ? DefaultSchema["Tables"][DefaultSchemaTableNameOrOptions] extends {
+        Update: infer U
+      }
+      ? U
+      : never
+    : never
+
+export type Enums<
+  DefaultSchemaEnumNameOrOptions extends
+    | keyof DefaultSchema["Enums"]
+    | { schema: keyof Database },
+  EnumName extends DefaultSchemaEnumNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"]
+    : never = never,
+> = DefaultSchemaEnumNameOrOptions extends { schema: keyof Database }
+  ? Database[DefaultSchemaEnumNameOrOptions["schema"]]["Enums"][EnumName]
+  : DefaultSchemaEnumNameOrOptions extends keyof DefaultSchema["Enums"]
+    ? DefaultSchema["Enums"][DefaultSchemaEnumNameOrOptions]
+    : never
+
+export type CompositeTypes<
+  PublicCompositeTypeNameOrOptions extends
+    | keyof DefaultSchema["CompositeTypes"]
+    | { schema: keyof Database },
+  CompositeTypeName extends PublicCompositeTypeNameOrOptions extends {
+    schema: keyof Database
+  }
+    ? keyof Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"]
+    : never = never,
+> = PublicCompositeTypeNameOrOptions extends { schema: keyof Database }
+  ? Database[PublicCompositeTypeNameOrOptions["schema"]]["CompositeTypes"][CompositeTypeName]
+  : PublicCompositeTypeNameOrOptions extends keyof DefaultSchema["CompositeTypes"]
+    ? DefaultSchema["CompositeTypes"][PublicCompositeTypeNameOrOptions]
+    : never
+
+export const Constants = {
+  public: {
+    Enums: {},
+  },
+} as const
